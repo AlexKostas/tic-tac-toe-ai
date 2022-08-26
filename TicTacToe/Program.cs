d90160f70deb0541b5d7cfb47cdf48f94f6d4fe4
AlexKostas <k.alexandros299@gmail.com>
2022-08-26 18:27:32 +0300

    Initial Commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
